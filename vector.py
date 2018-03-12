b07aed77c1dec86ac46f60f691fbe81a797a09b3
Sandra Arato <sandra@idoescode.com>
2018-03-13 07:51:24 +1000

    adding more functions for vector

--- vector.py
+++ vector.py
@@ -1,3 +1,4 @@
+import math
 class Vector(object):
     def __init__(self, coordinates):
         try:
@@ -19,4 +20,29 @@ class Vector(object):
 
     def __eq__(self, v):
         return self.coordinates == v.coordinates
+
+def add(a, b):
+    new_coordinates = [x + y for x, y in zip(a.coordinates, b.coordinates)]
+    vector = Vector(new_coordinates)
+    return vector
+
+def subtract(a, b):
+    new_coordinates = [x - y for x, y in zip(a.coordinates, b.coordinates)]
+    vector = Vector(new_coordinates)
+    return vector
+
+def scalar(a, scale):
+    coordinates = [coordinate * scale for coordinate in a.coordinates ]
+    vector = Vector(coordinates)
+    return vector
+
+def magnitude(a):
+    lengthSq = [math.pow(x, 2) for x in a.coordinates] 
+    return math.sqrt(sum(lengthSq))
+
+def normalize(a):
+    length = magnitude(a)
+    vector = scalar(a, (1 / length))
+    return vector
+
 
